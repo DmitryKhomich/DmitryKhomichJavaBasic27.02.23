9bd6bb7bb07ebbd28818488b87b3a80702890e65
dmitrykhomich <d.mastazm@gmail.com>
2023-03-03 13:09:06 +0200

    Add Homework 3. Coordinates

---
+++ src/hw3Сoordinates/Coordinates.java
@@ -0,0 +1,10 @@
+package hw3Сoordinates;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        double myLatitude, myLongitude;
+        myLatitude = 50.511193913122106;
+        myLongitude = 30.416923210658545;
+        System.out.println("My latitude: " + myLatitude + ";" + " My longitude: " + myLongitude);
+    }
+}
