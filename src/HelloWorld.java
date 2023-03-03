8c57015176600d0ac5069dfcbd483772e18ce039
dmitrykhomich <d.mastazm@gmail.com>
2023-03-03 12:06:31 +0200

    fix bugs

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
         int firstNumberInt = 0;
         int secondNumberInt = 0;
         double firstNumberDouble = 0;
-        double secondNumberDouble =0;
+        double secondNumberDouble = 0;
         String operation;
         boolean firstIsInt;
         boolean secondIsInt;
@@ -39,31 +39,72 @@ public class HelloWorld {
 
             switch (operation) {
                 case "+":
-                    if(firstIsInt == true && secondIsInt == true)
+                    if(firstIsInt && secondIsInt)
                         System.out.println("Результат: " + (firstNumberInt + secondNumberInt));
-                    if(firstIsInt == false && secondIsInt == false)
+                    if(!firstIsInt && !secondIsInt)
                         System.out.println("Результат: " + (firstNumberDouble + secondNumberDouble));
-                    if(firstIsInt == true && secondIsInt == false)
+                    if(firstIsInt && !secondIsInt)
                         System.out.println("Результат: " + (firstNumberInt + secondNumberDouble));
-                    if(firstIsInt == false && secondIsInt == true)
+                    if(!firstIsInt && secondIsInt)
                         System.out.println("Результат: " + (firstNumberDouble + secondNumberInt));
                     break;
                 case "-":
-                    System.out.println("Результат: " + (firstNumberInt - secondNumberInt));
+                    if(firstIsInt && secondIsInt)
+                        System.out.println("Результат: " + (firstNumberInt - secondNumberInt));
+                    if(!firstIsInt && !secondIsInt)
+                        System.out.println("Результат: " + (firstNumberDouble - secondNumberDouble));
+                    if(firstIsInt && !secondIsInt)
+                        System.out.println("Результат: " + (firstNumberInt - secondNumberDouble));
+                    if(!firstIsInt && secondIsInt)
+                        System.out.println("Результат: " + (firstNumberDouble - secondNumberInt));
                     break;
                 case "*":
-                    System.out.println("Результат: " + (firstNumberInt * secondNumberInt));
+                    if(firstIsInt && secondIsInt)
+                        System.out.println("Результат: " + (firstNumberInt * secondNumberInt));
+                    if(!firstIsInt && !secondIsInt)
+                        System.out.println("Результат: " + (firstNumberDouble * secondNumberDouble));
+                    if(firstIsInt && !secondIsInt)
+                        System.out.println("Результат: " + (firstNumberInt * secondNumberDouble));
+                    if(!firstIsInt && secondIsInt)
+                        System.out.println("Результат: " + (firstNumberDouble * secondNumberInt));
                     break;
                 case "/":
-                    if (secondNumberInt == 0){
+                    if (secondNumberInt == 0 && secondNumberDouble == 0){
                         throw new RuntimeException("Деление на 0 запрещено");
                     }
-                    if(firstNumberInt % secondNumberInt == 0)
-                    System.out.println("Результат: " + (firstNumberInt / secondNumberInt));
-                    else {
-                        double res = (double) firstNumberInt / secondNumberInt;
-                        System.out.println(res);
+                    if(firstIsInt && secondIsInt ) {
+                        if (firstNumberInt % secondNumberInt == 0)
+                            System.out.println("Результат: " + (firstNumberInt / secondNumberInt));
+                        else {
+                            double res = firstNumberInt / secondNumberInt;
+                            System.out.println(res);
+                        }
                     }
+                    if(!firstIsInt && !secondIsInt ) {
+                        if((firstNumberDouble % secondNumberDouble != 0))
+                        System.out.println("Результат: " + (firstNumberDouble / secondNumberDouble));
+                        else{
+                            int res = (int) (firstNumberDouble / secondNumberDouble);
+                            System.out.println(res);
+                        }
+                    }
+                    if(firstIsInt && !secondIsInt ) {
+                        if ((firstNumberInt % secondNumberDouble != 0))
+                            System.out.println("Результат: " + (firstNumberInt / secondNumberDouble));
+                        else {
+                            int res = (int) (firstNumberInt / secondNumberDouble);
+                            System.out.println(res);
+                        }
+                    }
+                        if (!firstIsInt && secondIsInt ) {
+                            if((firstNumberDouble % secondNumberDouble != 0))
+                            System.out.println("Результат: " + (firstNumberDouble / secondNumberInt));
+                        else{
+                                int res = (int) (firstNumberDouble / secondNumberInt);
+                                System.out.println(res);
+                            }
+                        }
+
                     break;
                 default:
                     throw new RuntimeException("К сожалению, вы ввели неправильную операцию над числами, доступные операции: +, -, *, /");
