a6290c2385fa23bab559935a6e1b981fb7c3f8b0
dmitrykhomich <d.mastazm@gmail.com>
2023-03-07 10:29:24 +0200

    hot fix

--- src/hw4Parallelepiped/Parallelepiped.java
+++ src/hw4Parallelepiped/Parallelepiped.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Parallelepiped {
     public static void main(String[] args) {
-        int length, width, height, volume;
+        int length, width, height, volume, lengthOfParallelepiped;
         System.out.println("Программа параллелепидет приветствует тебя!");
         System.out.println("Обозначь длину целым числом: ");
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNextInt())
-            length = scanner.nextInt();
+            lengthOfParallelepiped = scanner.nextInt();
         else {
             throw new RuntimeException("Вы ввели не целое число!");
         }
@@ -25,7 +25,9 @@ public class Parallelepiped {
         else {
             throw new RuntimeException("Вы ввели не целое число!");
         }
-        volume = width * length * height;
+        volume = width * lengthOfParallelepiped * height;
+        length = width + height + lengthOfParallelepiped;
         System.out.println("Объем параллелепипеда = " + volume);
+        System.out.println("Суммарная длина = " + length);
     }
 }
