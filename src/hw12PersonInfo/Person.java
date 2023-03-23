0a457785f57b57b86b4db1fc60e11b0c35cf4263
dmitrykhomich <d.mastazm@gmail.com>
2023-03-23 22:03:28 +0200

    add hw12PersonInfo

--- src/hw12PersonInfo/Person.java
+++ src/hw12PersonInfo/Person.java
@@ -2,13 +2,13 @@ package hw12PersonInfo;
 
 public class Person {
 
-    public static String personInfo(String name, String lastName, String city, String phoneNumber){
+    public static String personInfo(String name, String lastName, String city, long phoneNumber){
         return ("Позвоните гражданину " + name + " " + lastName + " " + "с города " + city + " можно по номеру " + phoneNumber);
     }
 
     public static void main(String[] args) {
-        System.out.println(personInfo("Will","Smith","New York","2936729462846"));
-        System.out.println(personInfo("Jackie","Chan","Shanghai","12312412412"));
-        System.out.println(personInfo("Sherlock","Holmes","London","37742123513"));
+        System.out.println(personInfo("Will","Smith","New York",2936729462846L));
+        System.out.println(personInfo("Jackie","Chan","Shanghai",12312412412L));
+        System.out.println(personInfo("Sherlock","Holmes","London",37742123513L));
     }
 }
